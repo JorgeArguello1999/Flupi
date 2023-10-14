4d84e79f17b8b84c6d19c5b77de9eed8d3e62c67
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-10-14 16:25:12 -0500

    feat: separe los comandos del código principal

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
-import datetime, os
 from modules import voice_text, text_voice, chatgpt
+from modules import comandos
+import os 
 
-# Estos son los comandos, si vas a añadir más comandos 
-# ver como entiende las ordenes el voice_text.py
 token = os.environ.get("GPT")
-nombre = "Maxi"
-hora = datetime.datetime.now().strftime("%H:%M")
-fecha_actual = datetime.datetime.now()
-
-nombres_meses = {
-    1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio", 
-    8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
-}
-fecha = f"{fecha_actual.day} de {nombres_meses[fecha_actual.month]} de {fecha_actual.year}"
-
-
-comandos = {
-    f"Hola {nombre}": "Hola, en que te puedo ayudar?",
-    f"{nombre} qué hora es": f"Hola, la hora es: {hora}",
-    f"{nombre} qué fecha es": f"La fecha es: {fecha}"
-}
-
-def functions(text:str):
-    for com in comandos:
-        if text in com:
-            return comandos[com]
+nombre = comandos.nombre
 
 while True:
     voz = voice_text.microphone()
-    comando = functions(voz)
+    comando = comandos.functions(voz)
     print(voz)
 
     if nombre in voz:
