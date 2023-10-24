a23631e5c1ea730b705d0a6e374ff8420af74ba7
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-10-24 16:10:20 -0500

    work: working on fuck interface

--- interface.py
+++ interface.py
@@ -1,28 +1,45 @@
 import flet as ft
+from flet import *
+import threading
+
+# Modulo de utilidades netas
+def start_utilities():
+    import main
+
+utilites = threading.Thread(target=start_utilities)
+utilites.start()
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.title = "Images Example"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.padding = 50
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
+    img = ft.Image(
+        src=f"./me/1.jpeg",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
     )
+    images = ft.Row(expand=1, wrap=True, scroll="always")
 
-ft.app(target=main)
+    page.add(img, images)
+
+    for i in range(0, 30):
+        images.controls.append(
+            ft.Image(
+                src=f"./me/{i}.jpeg",
+                width=200,
+                height=200,
+                fit=ft.ImageFit.NONE,
+                border_radius=ft.border_radius.all(10),
+            )
+        )
+    page.update()
+
+utilites.join()
+
+if __name__ == "__main__":
+    # Lanzamos la APP para escritorio
+    ft.app(target=main)
+    # Lanzamos la APP para web 
+    # ft.app(target=main, view=ft.AppView.WEB_BROWSER)
