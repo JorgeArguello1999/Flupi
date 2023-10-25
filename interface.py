f1c8d1e598b7c5753dfc0cfa130fe4570cc91e8c
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-10-25 13:22:22 -0500

    work: Trabajando en la interfaz gráfica trabajando en los eventos para activar ciertos videos

--- interface.py
+++ interface.py
@@ -1,35 +1,66 @@
 from kivy.lang import Builder
+from kivy.uix.image import Image
 from kivymd.app import MDApp
-from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.video import Video
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
+from kivy.config import Config
 
 class MainApp(MDApp):
     title = "Hola Maxi"
+    fonts = {
+        "junegull": "./me/junegull.ttf"
+    }
+
     def build(self):
         # Configuraciones principales
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
 
+        # Widget raíz 
+        root = FloatLayout()
+
+        # Imagen de fondo
+        background = Image(
+            source="./me/fondo.png",
+            allow_stretch=True,
+            keep_ratio=False
+        )
+
         # Mensaje
-        layout = BoxLayout(orientation='vertical', spacing=10)
+        layout = BoxLayout(
+            orientation='vertical', 
+            spacing=10,
+        )
 
         # Texto de bienvenida
-        welcome_label = Label(text="Hola, bienvenido, soy Maxi", font_size='30sp', halign='center', valign='middle')
+        welcome_label = Label(
+            text="Hola, bienvenido, soy Maxi", 
+            font_size='30sp', 
+            font_name=self.fonts["junegull"],
+            halign='center', 
+            valign='middle'
+        )
+
+        # Texto para saludar
+        greet_label = Label(
+            text="Di, hola Maxi", 
+            font_size='30sp', 
+            halign='center', 
+            valign='middle',
+            font_name=self.fonts["junegull"]
+        )
         
         # Reproductor de video
         player = Video(source="./me/video.mp4")
         player.state = "play"
         player.options = {"eos": "loop"}
         player.allow_fullscreen = True
-
-        # Texto para saludar
-        greet_label = Label(text="Di, hola Maxi", font_size='30sp', halign='center', valign='middle')
 
         # Botones
         buttons_layout = GridLayout(cols=2, spacing=10, size_hint=(1, None), height=50)
@@ -44,7 +75,11 @@ class MainApp(MDApp):
         buttons_layout.add_widget(exit_button)
         buttons_layout.add_widget(command_button)
 
-        return layout
+        # Agregar elementos al Layout
+        root.add_widget(background)
+        root.add_widget(layout)
+
+        return root
 
     def exit_app(self, instance):
         App.get_running_app().stop()
