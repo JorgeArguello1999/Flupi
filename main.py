1031b224f5b88864914331f69909af2c8921149a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-03 15:22:13 -0500

    feat: Envia audios, creando script para ESP32

--- main.py
+++ main.py
@@ -41,11 +41,10 @@ def chatbot_post():
     respuesta = chatbot.chatbot(pregunta)
 
     # En caso de ser para el bot, enviamos un Audio
-    """
-    if data["device"] == "bot":
+    if data["device"] != "computer":
         salida = voice.speaker(respuesta)
         return send_from_directory('static/audio_chatbot', salida)
-    """
+
     return respuesta 
 
 
