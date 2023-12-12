d0e54f5b27c8165b0dc967b0b7c37e83b6a734c0
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-12 10:01:16 -0500

    work: Trabajando en la API update: requirements con lo necesario

--- modules/database.py
+++ modules/database.py
@@ -14,17 +14,17 @@ def search_product(item:str, condicion:str = "and items.saldo>0")-> str:
         
         # Si la respuesta tiene contenido
         if len(response.text) > 3:
-            response = response.text
+            return response.text
         else:
-            response = "El código o el item no existe en la base de datos"
+            return "El código o el item no existe en la base de datos"
 
     except Exception as e:
         response = {
             "status": "Error",
             "error": e
         }
-    
-    return response
+
+        return response
 
 if __name__ == "__main__":
     salida = search_product("HP AIO")
