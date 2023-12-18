09f4141850e40fd4560ac67d70c43a117b66e09e
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-17 19:14:16 -0500

    work: Trabajando en la creación de contextos con base de datos

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Contextos
 
 # Register your models here.
+admin.site.register(Contextos)
--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Contextos:
+class Contextos(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     content = models.TextField()
