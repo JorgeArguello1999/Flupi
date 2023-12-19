ac6ce617441e5a035a17acce8e7f5a84e74d748a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-19 11:14:47 -0500

    feat: Funciona las imagenes con base de datos work: Trabajando en hacer modulos todas las aplicaciones

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 
 import os
 import datetime
+import sqlite3
 
 from modules import voice 
 from modules import chatbot
@@ -160,23 +161,50 @@ def read_context():
     context_files = context.get_context_all()
     return render_template('context.html', files=context_files)
 
-# Ruta para las imagenes
+# Ruta para obtener una imagen por su nombre
 @app.route('/images/<filename>', methods=['GET'])
 def get_image(filename):
-    content = context.get_image(filename)
-    return jsonify({'filename': filename, 'content': content})
+    conn = sqlite3.connect('alarm_status.db')
+    cursor = conn.cursor()
 
+    cursor.execute("SELECT image FROM Fotos WHERE name=?", (filename,))
+    image_data = cursor.fetchone()
+
+    conn.close()
+
+    if image_data:
+        return jsonify({'filename': filename, 'content': image_data[0]})
+    else:
+        return jsonify({'message': 'Image not found'})
+
+# Ruta para actualizar una imagen
 @app.route('/images/<filename>', methods=['POST'])
 def update_image(filename):
+    conn = sqlite3.connect('alarm_status.db')
+    cursor = conn.cursor()
+
     new_image = request.files['new_image']
-    context.update_image(filename, new_image)
+    new_image_data = new_image.read()
+
+    # Actualizar la imagen en la base de datos
+    cursor.execute("UPDATE Fotos SET image=? WHERE name=?", (new_image_data, filename))
+    conn.commit()
+    conn.close()
+
     return jsonify({'filename': filename, 'message': 'File updated successfully'})
 
+# Ruta para obtener la lista de imágenes
 @app.route('/images_f/', methods=['GET'])
 def read_images():
-    images_files = context.get_image_all()
-    return render_template('change_images.html', files=images_files)
+    conn = sqlite3.connect('alarm_status.db')
+    cursor = conn.cursor()
 
+    cursor.execute("SELECT name FROM Fotos")
+    image_names = [row[0] for row in cursor.fetchall()]
+
+    conn.close()
+
+    return render_template('change_images.html', files=image_names)
 
 if __name__ == "__main__":
     app.run(
