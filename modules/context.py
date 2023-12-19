865936128daef2754ddbd860383c5a310f3221e2
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-19 10:09:52 -0500

    feat: Funcion de actualizar el contexto añadida

--- modules/context.py
+++ modules/context.py
@@ -14,6 +14,26 @@ def get_context(name:str)-> str:
 
     return response
 
+def update_context(name:str, text:str)-> bool:
+    """
+    :name -> Contexto a modificar
+    :text -> Texto a Actualizar
+    """
+    try:
+        conn = sqlite3.connect('alarm_status.db')
+        cursor = conn.cursor()
+        cursor.execute(f'UPDATE ContextData SET content="{text}" WHERE name="{name}"')
+        conn.commit()
+        conn.close()
+        return True
+    
+    except Exception as e:
+        print(e)
+        return False
+
 if __name__ == "__main__":
-    salida = get_context("no_producto")
+    salida = get_context("error_mensaje")
     print(salida)
+
+    editar = update_context("error_mensaje", input("Ingresa el contexto: "))
+    print(editar)
