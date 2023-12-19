fbee4c1557ca4f26d74d4a619f4f92f657e72769
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-19 16:03:24 -0500

    feat: En el Imagenes ya se ve la foto de la base de datos work: Trabajando en que esta imagen se utilice en la respuestas

--- configs/routes.py
+++ configs/routes.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template
 from flask import request
 from flask import jsonify
 
-import sqlite3
+import base64
 
 from configs import context
+from configs import images
 
 configs_bp = Blueprint('configs_bp', __name__, url_prefix='/configs', template_folder='templates', static_folder='static')
 
@@ -37,47 +38,32 @@ def read_context():
     context_files = context.get_context_all()
     return render_template('context.html', files=context_files)
 
-# Ruta para obtener una imagen por su nombre
+
+# Rutas para las imagenes
 @configs_bp.route('/images/<filename>', methods=['GET'])
-def get_image(filename):
-    conn = sqlite3.connect('alarm_status.db')
-    cursor = conn.cursor()
+def get_image_route(filename):
+    image_data = images.get_image(filename)
 
-    cursor.execute("SELECT image FROM Fotos WHERE name=?", (filename,))
-    image_data = cursor.fetchone()
-
-    conn.close()
-
-    if image_data:
-        return jsonify({'filename': filename, 'content': image_data[0]})
+    if image_data != 'Image not found':
+        # Codificar la imagen en base64
+        encoded_image = base64.b64encode(image_data).decode('utf-8')
+        return jsonify({'filename': filename, 'content': encoded_image})
     else:
         return jsonify({'message': 'Image not found'})
 
-# Ruta para actualizar una imagen
 @configs_bp.route('/images/<filename>', methods=['POST'])
-def update_image(filename):
-    conn = sqlite3.connect('alarm_status.db')
-    cursor = conn.cursor()
-
+def update_image_route(filename):
     new_image = request.files['new_image']
     new_image_data = new_image.read()
 
-    # Actualizar la imagen en la base de datos
-    cursor.execute("UPDATE Fotos SET image=? WHERE name=?", (new_image_data, filename))
-    conn.commit()
-    conn.close()
+    result = images.update_image(filename, new_image_data)
 
-    return jsonify({'filename': filename, 'message': 'File updated successfully'})
+    if result == "Imagen actualizada correctamente en la base de datos.":
+        return jsonify({'filename': filename, 'message': 'File updated successfully'})
+    else:
+        return jsonify({'filename': filename, 'message': result})
 
-# Ruta para obtener la lista de imágenes
 @configs_bp.route('/images_f/', methods=['GET'])
-def read_images():
-    conn = sqlite3.connect('alarm_status.db')
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT name FROM Fotos")
-    image_names = [row[0] for row in cursor.fetchall()]
-
-    conn.close()
-
+def read_images_route():
+    image_names = images.get_image_all()
     return render_template('change_images.html', files=image_names)
