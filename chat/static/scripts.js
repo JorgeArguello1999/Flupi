86f40b5d2fc71324c3c73686d048fbd2906cd508
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-20 10:05:47 -0500

    feat: Separando en JS CSS

---
+++ chat/static/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    function getProfileImage(isUser, imageData) {
+        if (isUser) {
+            return `<img src="${convertBase64ToImage(imageData)}" alt="user_profile" class="img_profile">`;
+        } else {
+            return `<img src="${convertBase64ToImage(imageData)}" alt="chatbot_profile" class="img_profile">`;
+        }
+    }
+
+    function convertBase64ToImage(base64String) {
+        return `data:image/jpeg;base64,${base64String}`;
+    }
+
+    function showApiResponse(responseData) {
+        const response = responseData.response;
+        const role = responseData.role;
+        const photoUser = responseData.photo_user; // Imagen base64 del usuario
+        const photoRole = responseData.photo_role; // Imagen base64 del chatbot
+        const timeAnswer = responseData.time_answer; // Hora de la respuesta
+
+        let photo = "";
+
+        if(role == "assistant"){
+            photo = photoRole;
+        }
+        if(role == "user"){
+            photo = photoUser;
+        }
+        console.log(role);
+
+        const messageDiv = `<div class="message-bot">${getProfileImage(false, photo)}<p>${response}</p><span class="message-time">${timeAnswer}</span></div>`;
+        $("#chat-messages").prepend(messageDiv);
+
+        $("#chat-container").scrollTop(0);
+    }
+
+    function showMessage(message, isUser, photoUser) {
+        const sender = isUser ? 'Tú' : 'Chatbot';
+        const cssClass = isUser ? 'message-user' : 'message-bot';
+        const profileImg = getProfileImage(isUser, photoUser);
+        const currentTime = new Date().toLocaleTimeString(); // Obtener la hora actual
+        const messageDiv = `<div class="${cssClass}">${profileImg}<p>${message}</p><span class="message-time">${currentTime}</span></div>`;
+        $("#chat-messages").prepend(messageDiv);
+
+        $("#chat-container").scrollTop(0);
+    }
+
+    $("#chat-form").submit(function(event) {
+        event.preventDefault();
+        const userMessage = $("#message-input").val();
+        showMessage(userMessage, true, 'user');
+
+        $.ajax({
+            type: "POST",
+            url: "/api/",
+            data: JSON.stringify({ 
+            "ask": userMessage, 
+            "user": "Bot",
+            "device": "computer"
+        }),
+            contentType: "application/json",
+            dataType: "json",
+            success: function(data) {
+                showApiResponse(data);
+            }
+        });
+
+        $("#message-input").val("");
+    });
+
+    $("#message-input").focus();
+
+    $(".command").on("click", function(event) {
+        const selectedCommand = $(this).text();
+        $("#message-input").val(selectedCommand);
+    });
+});
