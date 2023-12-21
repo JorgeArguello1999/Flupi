1f0ab00c1c664f90adae03fe3da94c6f144ca41b
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-21 15:41:55 -0500

    feat: Ya funciona el Chatbot work: Trabajando en que se muestre mejor los mensajes

--- chat/static/scripts.js
+++ chat/static/scripts.js
@@ -3,15 +3,14 @@ $(document).ready(function() {
         $('#chat-messages').animate({ scrollTop: $('#chat-messages').prop('scrollHeight') }, 300);
     }
 
-    function showMessage(message, isUser, image) {
+    function showMessage(message, isUser, image, time) {
         const sender = isUser ? 'Tú' : 'Chatbot';
         const cssClass = isUser ? 'message-user' : 'message-bot';
-        const currentTime = new Date().toLocaleTimeString(); // Obtener la hora actual del computador
         const messageDiv = `
             <div class="${cssClass}">
                 <img src="data:image/png;base64,${image}" class="img_profile" />
                 <p>${message}</p>
-                <span class="message-time">${currentTime}</span>
+                <span class="message-time">${time}</span>
             </div>`;
         $("#chat-messages").append(messageDiv);
         scrollToBottom();
@@ -21,6 +20,7 @@ $(document).ready(function() {
     $("#chat-form").submit(function(event) {
         event.preventDefault();
         const userMessage = $("#message-input").val();
+        const currentTime = new Date().toLocaleTimeString(); // Obtener la hora actual del navegador
 
         fetch('/api/', {
             method: 'POST',
@@ -36,8 +36,8 @@ $(document).ready(function() {
         .then(response => response.json())
         .then(data => {
             // Manejar la respuesta de la API
-            showMessage(data.response, false, data.photo_role); // Suponiendo que 'data.botImage' contiene la imagen en base64 del bot
-            showMessage(userMessage, true, data.photo_user); // Suponiendo que 'data.userImage' contiene la imagen en base64 del usuario
+            showMessage(userMessage, true, data.photo_user, currentTime); // Mostrar la foto del usuario después de su mensaje
+            showMessage(data.response, false, data.photo_role, currentTime); // Mostrar la respuesta del chatbot después del mensaje del usuario
         })
         .catch(error => {
             console.error('Error:', error);
