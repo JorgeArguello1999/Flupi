565041eab2056eddfd9a204a78b8518dc76aa592
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-26 10:47:41 -0500

    feat: Trabajando con firebase, configuraciones iniciales

--- README.md
+++ README.md
@@ -21,8 +21,13 @@ Para usar el bot tienes que realizar los siguientes pasos:
 
 Estas son las variables importantes:
 ``` bash
-GPT=Your_OPENAI_TOKEN
-API_ROUTE=ROUTE_API_TO_CONSULT
+GPT=Tu token para GPT
+API_ROUTE=ruta_api_para_consultar_precios
 DEBUG=false
+FLASK_KEY=clave para flask
+
+ADMIN_KEY=esta frase es la que se necesita para eliminar usuarios
+NOMBRE= Con esto busca dentro de la base de datos
+JSON_CGS= Ruta donde esta el json para firebase
 
 ```
--- databases/firebase_example.py
+++ databases/firebase_example.py
@@ -1,21 +1,33 @@
+from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+
 import firebase_admin
-from firebase_admin import credentials, db
+import os 
+
+load_dotenv()
+nombre_proyecto = os.environ.get('NOMBRE') 
 
 # Inicializa la app de Firebase
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://your-project-id.firebaseio.com'
-})
+cred = credentials.Certificate(os.environ.get('JSON_CGS'))
+firebase_admin.initialize_app(cred)
 
-# Referencia a la base de datos
-ref = db.reference('/')
+# Obtiene una instancia de Firestore
+db = firestore.client()
 
-# Ejemplo de escritura de datos
-ref.child('users').child('user_id').set({
-    'name': 'John Doe',
-    'email': 'johndoe@example.com'
-})
+# Lista todas las colecciones en tu base de datos Firestore
+collections = db.collections()
 
-# Ejemplo de lectura de datos
-snapshot = ref.child('users').child('user_id').get()
-print(snapshot.val())  # Imprime los datos leídos
+# Referencia a la colección
+coleccion_ref = db.collection('contextos')
+
+# Obtiene todos los documentos de la colección
+documentos = coleccion_ref.stream()
+
+# Itera sobre los documentos e imprime su contenido
+for documento in documentos:
+    print(f'Documento ID: {documento.id}')
+    print(f'Datos: {documento.to_dict()}')
+
+
+def get_data():
+    pass
