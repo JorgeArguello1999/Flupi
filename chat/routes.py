49d0a4fc76671d76ab09cb67b3c134447248cb49
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-28 12:42:36 -0500

    feat: Token dinamico para el usuario chatbot

--- chat/routes.py
+++ chat/routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
 
+from databases import usuarios
+
 chat_bp = Blueprint('chatbot', __name__, url_prefix='/chatbot', template_folder='templates', static_folder='static')
 
 # Chatbot Front
 @chat_bp.route('/', methods=['GET'])
 def chatbot_get():
-    return render_template('chatbot.html')
+    try:
+        chatbot = usuarios.get_user_by_username('chatbot')[1]
+    except:
+        chatbot = "No existe usuario"
+    return render_template('chatbot.html', token=chatbot)
