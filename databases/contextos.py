7a2c83cb5c6eb6b873698fb9726eaccafa96acc3
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-12-29 16:02:36 -0500

    feat: Los cambios de contexto solo se pueden hacer desde la pagina

--- databases/contextos.py
+++ databases/contextos.py
@@ -1,31 +1,51 @@
-from databases import FireStoreBase
+try:
+    from databases import FireStoreBase
+except:
+    import FireStoreBase
 
 # Iniciamos el objeto para contextos
 contextos = FireStoreBase.Firestore('contextos')
 
-def get_context_all() -> list:
+# Variables globales para almacenar los valores de la base de datos
+context_all_values = []  # Almacena todos los valores
+context_values = {}      # Almacena los valores por clave
+
+# Función para cargar los valores de la base de datos en las variables globales
+def load_context_values():
+    global context_all_values
+    global context_values
+
     response = contextos.get_value('mensajes')
 
     if response:
-        response = response.keys()
-        return [ i for  i in response ]
+        context_all_values = list(response.keys())
+        context_values = response
     else:
-        return ['Problema con la base de datos']
+        context_all_values = ['Problema con la base de datos']
+        context_values = {}
+
+def get_context_all() -> list:
+    return context_all_values
 
 def get_context(key:str) -> str:
-    response = contextos.get_value('mensajes')
-    if response: 
-        return response.get(key)
-    else:
-        return 'Error: Con la base de datos'
+    return context_values.get(key, 'Error: Con la base de datos')
 
 def update_context(name:str, text:str) -> bool:
-    data = {f'mensajes.{name}':text }
+    data = {f'mensajes.{name}': text}
     response = contextos.update_create_registry(data)
+    if response:
+        # Si la actualización fue exitosa, actualiza las variables globales
+        load_context_values()
     return response
 
-
 if __name__ == "__main__":
+    # Cargar los valores de la base de datos en las variables globales al inicio
+    load_context_values()
     print(get_context_all())
-    print(update_context('general', 'amilia'))
+    # print(update_context('general', 'amilia'))
     print(get_context('general'))
+
+else:
+
+    load_context_values()
+    print(get_context_all())
