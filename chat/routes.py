db51aa4a9454cdc61aec52cb24482105af946b09
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-15 17:23:31 -0500

    feat: Servicios de chat funcionando a traves de la ruta publica

--- chat/routes.py
+++ chat/routes.py
@@ -12,11 +12,7 @@ chat_bp = Blueprint('chatbot', __name__, url_prefix='/chatbot', template_folder=
 # Chatbot Front
 @chat_bp.route('/', methods=['GET'])
 def chatbot_get():
-    try:
-        chatbot = usuarios.get_user_by_username('chatbot')[1]
-    except:
-        chatbot = "No existe usuario"
-    return render_template('chatbot.html', token=chatbot)
+    return render_template('chatbot.html')
 
 
 # Chatbot Back
