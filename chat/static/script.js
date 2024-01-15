c2c21b75383cd47068f05571a8f24aed2d771214
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-15 17:32:45 -0500

    feat: Separando Js de la plantilla

---
+++ chat/static/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    function scrollToBottom() {
+        $('#chat-messages').animate({ scrollTop: $('#chat-messages').prop('scrollHeight') }, 300);
+    }
+
+    function showMessage(message, isUser, image, time) {
+        const sender = isUser ? 'Tú' : 'Chatbot';
+        const cssClass = isUser ? 'message-user' : 'message-bot';
+        const messageDiv = `
+            <div class="${cssClass}">
+                <img src="data:image/png;base64,${image}" class="img_profile" />
+                <p>${message}</p>
+                <span class="message-time">${time}</span>
+            </div>`;
+        $("#chat-messages").append(messageDiv);
+        scrollToBottom();
+    }
+    var apiUrl = window.location.origin;
+    apiUrl = apiUrl+'/chatbot/';
+    console.log(apiUrl);
+
+    // Intercepta el envío del formulario
+    $("#chat-form").submit(function(event) {
+        event.preventDefault();
+        const userMessage = $("#message-input").val();
+        const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "ask": userMessage
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Manejar la respuesta de la API
+            showMessage(userMessage, true, data.photo_user, currentTime); // Mostrar la foto del usuario después de su mensaje
+            showMessage(data.response, false, data.photo_role, currentTime); // Mostrar la respuesta del chatbot después del mensaje del usuario
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+        $("#message-input").val("");
+    });
+
+    scrollToBottom();
+    $("#message-input").focus();
+});
