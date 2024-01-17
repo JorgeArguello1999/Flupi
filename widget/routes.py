9e8c09ed36d11fc00ec6dad84cacb9f5c7de6d67
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-17 11:26:44 -0500

    feat: Semi Proxy funcionando

--- widget/routes.py
+++ widget/routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint 
+from flask import request
+from flask import jsonify
 
 from dotenv import load_dotenv
 load_dotenv()
 
+from chat.routes import chatbot_post
+
 widget_bp = Blueprint('widget', __name__, url_prefix='/widget', template_folder='templates', static_folder='static')
 
-
+@widget_bp.route('/', methods=['POST'])
+def widget_post():
+    return chatbot_post()
