6118919891190eb76c5043c6497d5558fcb030c5
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-18 12:18:19 -0500

    feat: Mejorando estilos para Widget

--- widget/static/script_widget.js
+++ widget/static/script_widget.js
@@ -27,6 +27,10 @@ function init(apiUrl){
         function showMessage(message, isUser, image, time) {
             const sender = isUser ? 'Tú' : 'Chatbot';
             const cssClass = isUser ? 'message-user' : 'message-bot';
+
+            // Eliminar el mensaje anterior del usuario si existe
+            chatMessages.find('.message-user:contains("' + message + '")').remove();
+
             const messageDiv = `
                 <div class="${cssClass}">
                 <img src="data:image/png;base64,${image}" class="img_profile" />
@@ -47,10 +51,13 @@ function init(apiUrl){
             const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
             // Limitar la longitud del contexto y crear un resumen
-            const contexto = `Con base a estas interacciones: ${list.slice(-5).join(';')} > Responde unicamente la pregunta actual: ${userMessage}`;
+            const contexto = `Con base a estas interacciones: ${list.slice(-5).join(';')} > Responde únicamente la pregunta actual: ${userMessage}`;
 
             // Guardamos las preguntas en la lista para generar un contexto
             list.push(userMessage);
+
+            // Mostrar mensaje del usuario originalmente enviado
+            showMessage(userMessage, true, null, currentTime);
 
             // Mostrar mensaje de escritura del bot
             showMessage("Escribiendo...", false, null, currentTime);
@@ -69,9 +76,17 @@ function init(apiUrl){
                     // Eliminar el mensaje de escritura del bot
                     chatMessages.find('.message-bot:contains("Escribiendo...")').remove();
 
-                    // Manejar la respuesta de la API
-                    showMessage(userMessage, true, data.photo_user, currentTime);
+                    // Mostrar mensaje de respuesta de la API con la foto y la pregunta
                     showMessage(data.response, false, data.photo_role, currentTime);
+
+                    // Actualizar el mensaje del usuario con la información de la API
+                    chatMessages.find('.message-user:contains("' + userMessage + '")').html(`
+                        <img src="data:image/png;base64,${data.photo_user}" class="img_profile" />
+                        <p>${data.question}</p>
+                        <span class="message-time">${currentTime}</span>
+                    `);
+
+                    scrollToBottom();
                 })
                 .catch(error => {
                     // Eliminar el mensaje de escritura del bot en caso de error
