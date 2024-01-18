673ac1dd041906039a684d0c5f038557cce472a6
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-18 15:42:52 -0500

    feat: Ruta para las imagenes creada

--- widget/routes.py
+++ widget/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint 
-from flask import request
 from flask import jsonify
 
 from dotenv import load_dotenv
 load_dotenv()
+
+from databases import images
 
 from chat.routes import chatbot_post
 
@@ -12,3 +13,22 @@ widget_bp = Blueprint('widget', __name__, url_prefix='/widget', template_folder=
 @widget_bp.route('/', methods=['POST'])
 def widget_post():
     return chatbot_post()
+
+@widget_bp.route('photos/', methods=['GET'])
+def widget_get():
+    try:
+        photo_chatbot = images.get_image('chatbot')
+    except Exception as e:
+        print("Error: ", e)
+        photo_chatbot = ''
+    
+    try:
+        photo_user = images.get_image('usuario')
+    except Exception as e:
+        print("Error: ", e)
+        photo_user = ''
+    
+    return jsonify({
+        "photo_user": photo_user,
+        "photo_role": photo_chatbot
+    })
