0bf294cbd7cabf072e8c5225600fd38da82f2f62
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-22 11:45:49 -0500

    feat: Modulo para enviar mensajes a telegram

---
+++ configs/telegram.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+import os 
+
+bot_token = os.getenv('TELEGRAM_TOKEN') 
+chat_id = os.getenv('CHAT_ID') 
+url = f'https://api.telegram.org/bot{bot_token}/sendMessage' 
+
+def send_message(message:str) -> bool:
+    params = {'chat_id': chat_id, 'text': message}
+
+    response = requests.post(url, params=params)
+    return True
+
+if __name__ == "__main__":
+    send_message("Bienvenido")
