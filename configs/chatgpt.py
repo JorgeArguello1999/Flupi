1234ce84a7e60df1b175cf556525c4c7c8a52e39
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-22 13:29:34 -0500

    feat: Actualizando modelo para poder usar mayor contexto feat: Boton del Widget en todas las pestañas

--- configs/chatgpt.py
+++ configs/chatgpt.py
@@ -14,7 +14,7 @@ def answer(user:str="anonym", context:str="", ask:str=""):
     """
 
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-1106",
         messages= [
             {"role": "system", "content": context},
             {"role": "user", "content": ask}
